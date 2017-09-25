6c5ab25e1f89e3cde0f4cd85ee9f3e2ae4f52c33
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2017-09-25 11:50:43 +0200

    :enh: use /dev/urandom for tomcat startup

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -5,6 +5,10 @@ mkdir -p /opt/apache-tomcat/conf
 cp -R /tmp/apache-tomcat*/* /opt/apache-tomcat
 rm -rf /opt/apache-tomcat/webapps/*
 
+cat <<-'EOF' > /opt/apache-tomcat/bin/setenv.sh
+JAVA_OPTS=-Djava.security.egd=file:/dev/./urandom
+EOF
+
 cat <<-'EOF' > /opt/apache-tomcat/conf/logging.properties
 	handlers = java.util.logging.ConsoleHandler
 	.handlers = java.util.logging.ConsoleHandler
