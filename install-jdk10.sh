2643815bec0a8f4eba9f7f288ff2775802772cd6
Roberto Ferranti <roberto.ferranti@gmail.com>
2018-06-25 12:29:44 +0200

    :new: jdk10

--- install-jdk10.sh
+++ install-jdk10.sh
@@ -6,26 +6,22 @@ if [ ! -f /usr/sbin/update-alternatives ]; then
 fi
 
 mkdir -p /usr/java
-cp -R /tmp/jdk1.8* /usr/java/
-jdkdir=$(readlink -f /usr/java/jdk1.8*)
+cp -R /tmp/jdk-10* /usr/java/
+jdkdir=$(readlink -f /usr/java/jdk-10*)
 jdkbindir=$(readlink -f ${jdkdir}/bin)
 
 chown -R root:root ${jdkdir}
 
 echo "installing alternatives (java) for oracle jdk"
 update-alternatives --install \
-	/usr/bin/java java $jdkbindir/java 1${MAJOR_VERSION}0${MINOR_VERSION} \
+	/usr/bin/java java $jdkbindir/java 10 \
 	--slave /usr/bin/javac javac $jdkbindir/javac \
-	--slave /usr/bin/ControlPanel ControlPanel $jdkbindir/ControlPanel \
 	--slave /usr/bin/appletviewer appletviewer $jdkbindir/appletviewer \
-	--slave /usr/bin/extcheck extcheck $jdkbindir/extcheck \
 	--slave /usr/bin/idlj idlj $jdkbindir/idlj \
+    --slave /usr/bin/jaotc jaotc $jdkbindir/jaotc \
 	--slave /usr/bin/jar jar $jdkbindir/jar \
 	--slave /usr/bin/jarsigner jarsigner $jdkbindir/jarsigner \
-	--slave /usr/bin/java-rmi.cgi java-rmi.cgi $jdkbindir/java-rmi.cgi \
 	--slave /usr/bin/javadoc javadoc $jdkbindir/javadoc \
-	--slave /usr/bin/javafxpackager javafxpackager $jdkbindir/javafxpackager \
-	--slave /usr/bin/javah javah $jdkbindir/javah \
 	--slave /usr/bin/javap javap $jdkbindir/javap \
 	--slave /usr/bin/javapackager javapackager $jdkbindir/javapackager \
 	--slave /usr/bin/javaws javaws $jdkbindir/javaws \
@@ -33,25 +29,26 @@ update-alternatives --install \
 	--slave /usr/bin/jconsole jconsole $jdkbindir/jconsole \
 	--slave /usr/bin/jcontrol jcontrol $jdkbindir/jcontrol \
 	--slave /usr/bin/jdb jdb $jdkbindir/jdb \
+    --slave /usr/bin/jdeprscan jdeprscan $jdkbindir/jdeprscan \
 	--slave /usr/bin/jdeps jdeps $jdkbindir/jdeps \
-	--slave /usr/bin/jhat jhat $jdkbindir/jhat \
+    --slave /usr/bin/jhsdb jhsdb $jdkbindir/jhsdb \
+    --slave /usr/bin/jimage jimage $jdkbindir/jimage \
 	--slave /usr/bin/jinfo jinfo $jdkbindir/jinfo \
 	--slave /usr/bin/jjs jjs $jdkbindir/jjs \
+    --slave /usr/bin/jlink jlink $jdkbindir/jlink \
 	--slave /usr/bin/jmap jmap $jdkbindir/jmap \
 	--slave /usr/bin/jmc jmc $jdkbindir/jmc \
-	--slave /usr/bin/jmc.ini jmc.ini $jdkbindir/jmc.ini \
+    --slave /usr/bin/jmod jmod $jdkbindir/jmod \
 	--slave /usr/bin/jps jps $jdkbindir/jps \
 	--slave /usr/bin/jrunscript jrunscript $jdkbindir/jrunscript \
-	--slave /usr/bin/jsadebugd jsadebugd $jdkbindir/jsadebugd \
+    --slave /usr/bin/jshell jshell $jdkbindir/jshell \
 	--slave /usr/bin/jstack jstack $jdkbindir/jstack \
 	--slave /usr/bin/jstat jstat $jdkbindir/jstat \
 	--slave /usr/bin/jstatd jstatd $jdkbindir/jstatd \
-	--slave /usr/bin/jvisualvm jvisualvm $jdkbindir/jvisualvm \
+    --slave /usr/bin/jweblauncher jweblauncher $jdkbindir/jweblauncher \
 	--slave /usr/bin/keytool keytool $jdkbindir/keytool \
-	--slave /usr/bin/native2ascii native2ascii $jdkbindir/native2ascii \
 	--slave /usr/bin/orbd orbd $jdkbindir/orbd \
 	--slave /usr/bin/pack200 pack200 $jdkbindir/pack200 \
-	--slave /usr/bin/policytool policytool $jdkbindir/policytool \
 	--slave /usr/bin/rmic rmic $jdkbindir/rmic \
 	--slave /usr/bin/rmid rmid $jdkbindir/rmid \
 	--slave /usr/bin/rmiregistry rmiregistry $jdkbindir/rmiregistry \
