7f878bd7c6ea0c48aa50c5486714b0d18499b09d
Roberto Ferranti <roberto.ferranti@gmail.com>
2018-07-02 17:36:08 +0200

    :fix: mysqld initialization

--- init-mariadb10.sh
+++ init-mariadb10.sh
@@ -4,11 +4,37 @@ if [ $# -eq 0 ]; then
     chown -R mysql:mysql "/var/lib/mysql"
     if [ ! -d "/var/lib/mysql/mysql" ]; then
         echo "initializing database"
-        gosu mysql mysql_install_db --datadir="/var/lib/mysql" >/dev/null 2>&1
+        gosu mysql mysql_install_db --datadir="/var/lib/mysql"
         echo "database initialized"
-        cat /sql-init.d/*.sql | perl -0777 -pe 's/^\s*#.*\n//gm' | perl -0777 -pe 's/([^\n;])[\r\t\n ]+/\1 /gm' | perl -0777 -pe 's/\n{2,}/\n/g' > /tmp/mariadb-first-time.sql
-        echo "executing first time initialization"
-        exec gosu mysql mysqld --defaults-file=/etc/my.cnf --init-file=/tmp/mariadb-first-time.sql
+
+        mysql_client=( mysql --protocol=socket -uroot )
+        echo "database initialized"
+        gosu mysql mysqld --defaults-file=/etc/my.cnf --skip-networking &
+        pid="$!"
+        for i in {30..0}; do
+			if echo 'select 1' | "${mysql_client[@]}" &> /dev/null; then
+				break
+			fi
+			echo 'waiting for server to start...'
+			sleep 1
+		done
+
+        for f in /sql-init.d/*; do
+			case "$f" in
+				*.sh)     echo "running $f"; . "$f" ;;
+				*.sql)    echo "running $f"; "${mysql_client[@]}" < "$f"; echo ;;
+				*.sql.gz) echo "running $f"; gunzip -c "$f" | "${mysql_client[@]}"; echo ;;
+                /sql-init.d/*) echo "no scripts found in /sql-init.d/" ;;
+				*)        echo "ignoring $f" ;;
+			esac
+			echo
+		done
+
+        if ! kill -s TERM "$pid" || ! wait "$pid"; then
+			echo >&2 'initialization failed.'
+			exit 1
+		fi
+
     fi
     exec gosu mysql mysqld --defaults-file=/etc/my.cnf
 fi
