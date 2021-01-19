dab2b3f4df3232e21065dd335f32b3f639c157b8
Roberto Ferranti <roberto.ferranti@gmail.com>
2021-01-19 13:37:24 +0100

    :new: git in -builder machines

--- install-builder.sh
+++ install-builder.sh
@@ -16,13 +16,13 @@ ln -s /opt/apache-maven/bin/mvn /usr/sbin/mvn
 case "${DISTRIB_LABEL}" in
     debian*|ubuntu*)
         DEBIAN_FRONTEND=noninteractive apt-get -y -q update
-        DEBIAN_FRONTEND=noninteractive apt-get -y -q install ansible make
+        DEBIAN_FRONTEND=noninteractive apt-get -y -q install ansible make git
         DEBIAN_FRONTEND=noninteractive apt-get -y -q autoclean
         DEBIAN_FRONTEND=noninteractive apt-get -y -q autoremove
         rm -rf /var/lib/apt/lists/* /etc/apt/sources.list.d/nginx.list
     ;;
     centos8)
-        yum install -q -y centos-release-ansible-29 make which
+        yum install -q -y centos-release-ansible-29 make which git
         yum clean all
         rm -rf /var/cache/yum
     ;;
