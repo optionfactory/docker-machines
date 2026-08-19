a5abae4d74e0b593015e4f6f565287c703975509
Roberto Ferranti <roberto.ferranti@gmail.com>
2026-08-19 18:59:18 +0200

    :ref: removed old branches to support rocky9

--- scripts/install-builder.sh
+++ scripts/install-builder.sh
@@ -2,34 +2,15 @@
 
 echo "Installing builder"
 
-
-
 groupadd --system --gid 950 builder
 useradd --system --create-home --gid builder --uid 950 builder
-
-
 
 mkdir -p /opt/apache-maven
 cp -R /build/apache-maven*/* /opt/apache-maven
 ln -s /opt/apache-maven/bin/mvn /usr/sbin/mvn
 
-#
-
-case "${DISTRIB_LABEL}" in
-    debian*|ubuntu*)
-        DEBIAN_FRONTEND=noninteractive apt-get -y -q update
-        DEBIAN_FRONTEND=noninteractive apt-get -y -q install ansible make git rsync curl
-        DEBIAN_FRONTEND=noninteractive apt-get -y -q autoclean
-        DEBIAN_FRONTEND=noninteractive apt-get -y -q autoremove
-        rm -rf /var/lib/apt/lists/* /etc/apt/sources.list.d/*
-    ;;
-    rocky9)
-        yum install -q -y epel-release ansible-core make which git rsync
-        yum clean all
-        rm -rf /var/cache/yum
-    ;;
-    *)
-    echo "distribution ${DISTRIB_LABEL} not supported"
-    exit 1
-    ;;
-esac
+DEBIAN_FRONTEND=noninteractive apt-get -y -q update
+DEBIAN_FRONTEND=noninteractive apt-get -y -q install ansible make git rsync curl
+DEBIAN_FRONTEND=noninteractive apt-get -y -q autoclean
+DEBIAN_FRONTEND=noninteractive apt-get -y -q autoremove
+rm -rf /var/lib/apt/lists/* /etc/apt/sources.list.d/*
--- scripts/install-journal-webd.sh
+++ scripts/install-journal-webd.sh
@@ -2,24 +2,11 @@
 
 echo "Installing journal-webd"
 
-case "${DISTRIB_LABEL}" in
-    debian*|ubuntu*)
-        DEBIAN_FRONTEND=noninteractive apt-get -y -q update
-        DEBIAN_FRONTEND=noninteractive apt-get -y -q --no-install-recommends install systemd-journal-remote
-        DEBIAN_FRONTEND=noninteractive apt-get -y -q autoclean
-        DEBIAN_FRONTEND=noninteractive apt-get -y -q autoremove
-        rm -rf /var/lib/apt/lists/*
-    ;;
-    rocky9)
-        yum install -q -y systemd-journal-remote
-        yum clean all
-        rm -rf /var/cache/yum
-    ;;
-    *)
-        echo "distribution ${DISTRIB_LABEL} not supported"
-        exit 1
-    ;;
-esac
+DEBIAN_FRONTEND=noninteractive apt-get -y -q update
+DEBIAN_FRONTEND=noninteractive apt-get -y -q --no-install-recommends install systemd-journal-remote
+DEBIAN_FRONTEND=noninteractive apt-get -y -q autoclean
+DEBIAN_FRONTEND=noninteractive apt-get -y -q autoremove
+rm -rf /var/lib/apt/lists/*
 
 mkdir -p /journal-webd-conf/
 mkdir -p /journal-webd-logs/
